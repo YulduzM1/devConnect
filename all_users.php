c2f4dec82bd0db44c9de321df3dfa818447805b8
rrios1714 <rrbyam4711@gmail.com>
2024-02-03 21:40:14 -0800

    fixed all_users php
    fixed the file, the code should show up now

---
+++ all_users.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul class="nav-links">
+            <li><a href="login.html">Home</a></li>
+            <li><a href="aboutus.html">AboutUS</a></li>
+            <li><a href="how_it_works.html">How It works</a></li>
+            <li><a href="contact_us.html">Contact Us</a></li>
+        </ul>
+    </nav>
+</header>
